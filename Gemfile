d68644f7c59a439dd2e490b3b1dfb5a65ed578d4
Vladimir Saric <vladimir@renderedtext.com>
2011-04-05 11:32:18 +0200

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+gem 'pg', :require => 'pg'
+
+gem 'devise'
+gem 'kaminari'
+gem 'carrierwave'
+gem 'jquery-rails', '>= 0.2.6'
+
+group :test, :development do
+  gem 'rspec-rails', "~> 2.4"
+  gem 'cucumber-rails'
+  gem 'capybara'
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+  gem 'ruby-debug19'
+  gem 'mongrel', '>= 1.2.0.pre2'
+  gem 'chronic'
+  gem 'awesome_print'
+end
+
+group :test do
+  gem 'email_spec'
+  gem 'shoulda'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaseApp::Application.config.secret_token = '187c3a1a542c273651401faf9be7e8142500c678170398c3a561c37621938038f69640a66cdab4e253819c058f78217a05e4a81321ca509f9cc67a6f74e60e44'
