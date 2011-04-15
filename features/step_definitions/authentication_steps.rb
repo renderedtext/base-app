6174cab2b47ea1ed44628f3ed356ca0f9ca623e8
Vladimir Saric <vladimir@renderedtext.com>
2011-04-15 16:44:04 +0200

    Add 'I am admin' & 'I am logged in as admin' steps.

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -1,3 +1,19 @@
 Given /^I am registered$/ do
   @registered_user = Factory(:user, :email => "john@doe.com")
 end
+
+Given /^I am admin$/ do
+  @registered_user.make_admin
+end
+
+Given /^I am logged in as admin$/ do
+  steps %Q{
+    Given I am registered
+    And I am admin
+    And I am on the homepage
+    When I follow "Sign in"
+    And I fill in "Email" with "john@doe.com"
+    And I fill in "Password" with "password"
+    And I press "Sign in"
+  }
+end
