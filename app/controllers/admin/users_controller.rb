eb6c93c4b93466f530ce57b42021e1587324b876
Vladimir Saric <vladimir@renderedtext.com>
2011-04-18 12:18:03 +0200

    Add admin users list table sorting.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,25 @@
 class Admin::UsersController < Admin::BaseController
 
+  helper_method :sort_column, :sort_direction
+
   def index
-    @users = User.all
+    @users = User.order(sort_column + " " + sort_direction)
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def sort_column
+    User.column_names.include?(params[:sort]) ? params[:sort] : "email"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
 end
