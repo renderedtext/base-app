cf7d6447a205bc7f3274bedfb6cafe26ab3b66c2
Vladimir Saric <vladimir@renderedtext.com>
2011-04-18 17:56:54 +0200

    Set up basic Facebook login thru omniauth.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  devise :omniauthable
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
@@ -23,6 +25,15 @@ class User < ActiveRecord::Base
 
   def admin?
     role?(:admin)
+  end
+
+  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
+    data = access_token['extra']['user_hash']
+    if user = User.find_by_email(data["email"])
+      user
+    else # Create a user with a stub password.
+      User.create!(:email => data["email"], :password => Devise.friendly_token[0,20])
+    end
   end
 
 end
