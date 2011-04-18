075496b6cfcdc4bbf3b52b97b12f6b3bf06216e1
Vladimir Saric <vladimir@renderedtext.com>
2011-04-18 12:17:31 +0200

    Add helper for table sorting by column.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
+    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+    link_to title, { :sort => column, :direction => direction }, { :class => css_class }
+  end
+
 end
