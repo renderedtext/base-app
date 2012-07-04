926c98af4c14a1f5f5552187a934928f64f3186a
Vladimir Saric <vladimir@renderedtext.com>
2012-07-04 12:56:30 +0200

    If the 'admin' role does not exists create it.

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -6,4 +6,14 @@ describe Role do
 
   it { should have_and_belong_to_many(:users) }
 
+  describe ".admin" do
+
+    it "returns role with name 'admin'" do
+      role = Role.admin
+
+      role.name.should == "admin"
+    end
+
+  end
+
 end
