6681a8edeeadb65e1b1c43792b1d306d4b1f2ab4
Vladimir Saric <vladimir@renderedtext.com>
2013-09-13 10:46:25 +0200

    Add 'logged in' Cucumber step definition.

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -6,6 +6,17 @@ Given /^I am admin$/ do
   @registered_user.make_admin
 end
 
+Given /^I am logged in$/ do
+  steps %Q{
+    Given I am registered
+    And I am on the homepage
+    When I follow "Sign in"
+    And I fill in "Email" with "john@doe.com"
+    And I fill in "Password" with "password"
+    And I press "Sign in"
+  }
+end
+
 Given /^I am logged in as admin$/ do
   steps %Q{
     Given I am registered
@@ -16,9 +27,4 @@ Given /^I am logged in as admin$/ do
     And I fill in "Password" with "password"
     And I press "Sign in"
   }
-end
-
-When /^I fill in password fields with "(.*?)"$/ do |value|
-  find("#user_password").set(value)
-  find("#user_password_confirmation").set(value)
 end
