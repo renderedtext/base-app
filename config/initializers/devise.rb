f6effad18b3d68cf37665531785211d092deb60c
Marko Anastasov <marko@renderedtext.com>
2014-02-08 22:45:42 +0100

    Introduce DEVISE_SECRET_KEY.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -180,5 +180,5 @@ Devise.setup do |config|
   #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
   # end
 
-  config.secret_key = '696acf756fc36cde5c1654ade4ff0449fc095863a4bdccbc6cb55f5321d7268337128e5deee3771d5bee26a21f31bd160b2e91dfdec9dcd4d354b5e9404b6455'
+  config.secret_key = ENV["DEVISE_SECRET_KEY"] || '696acf756fc36cde5c1654ade4ff0449fc095863a4bdccbc6cb55f5321d7268337128e5deee3771d5bee26a21f31bd160b2e91dfdec9dcd4d354b5e9404b6455'
 end
